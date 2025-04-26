668e0e1ef9512d4e62f8cdb34ad6113d7c451443
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2025-04-26 16:12:03 +0300

    new: train implementation using a normal list

---
+++ 06_how_to_write_tests/TrainBuilder/src/ListTrain.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using OneOf;
+using OneOf.Types;
+
+namespace TrainBuilder;
+
+public class ListTrain(IValidator<InsertionContext> insertionValidator) : ITrain
+{
+    private readonly List<ICarriage> _carriages = [];
+
+    public OneOf<Success, Error<string>> InsertCarriage(ICarriage carriage, int idx)
+    {
+        throw new NotImplementedException();
+    }
+
+    public OneOf<List<ICarriage>, IdxOutOfRange> GetFromIdxInclusive(int idx)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Count => _carriages.Count;
+
+    public OneOf<string, EmptyTrain> Print
+    {
+        get
+        {
+            if (_carriages.Count == 0)
+                return new EmptyTrain();
+
+            return string.Empty;
+        }
+    }
+}
---
+++ 06_how_to_write_tests/TrainBuilder/tests/ListTrainTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using NSubstitute;
+
+namespace TrainBuilder.tests;
+
+public class ListTrainTests
+{
+    private IValidator<InsertionContext> _validator;
+    private ITrain _train;
+
+    [SetUp]
+    public void Setup()
+    {
+        _validator = Substitute.For<IValidator<InsertionContext>>();
+        _train = new ListTrain(_validator);
+    }
+}
