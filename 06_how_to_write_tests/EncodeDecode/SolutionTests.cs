d60696936e54cfb1c9c35754a3d3448b2cf779fc
Aleksandr Zakharov <aleks.zakharov@protonmail.com>
2025-04-27 22:42:44 +0300

    test(6): encode decode with long string with spaces

--- 06_how_to_write_tests/EncodeDecode/SolutionTests.cs
+++ 06_how_to_write_tests/EncodeDecode/SolutionTests.cs
@@ -17,5 +17,10 @@ public class SolutionTests
         yield return new TestCaseData(new List<string> { "" });
         yield return new TestCaseData(new List<string> { "test" });
         yield return new TestCaseData(new List<string> { "hello", "my", "friend", "!" });
+        yield return new TestCaseData(new List<string>
+        {
+            "leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode leetcode",
+            "is cool is cool is cool is cool is cool is cool is cool is cool is cool", "is cool is cool is cool is cool is coolis cool"
+        });
     }
 }
