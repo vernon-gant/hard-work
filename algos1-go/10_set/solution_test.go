55627d01e6de6d7da13cfd14c3552f75f210fb7d
Aleksandr Zakharov <aleks.zakharov@pm.me>
2026-01-22 00:54:10 +0100

    algos-go(set): GivenFullSet_WhenPuttingValues_ThenSizeDoesNotChangeAndMethodReturns

--- algos1-go/10_set/solution_test.go
+++ algos1-go/10_set/solution_test.go
@@ -57,3 +57,23 @@ func Test_GivenNonEmptySet_WhenPuttingDuplicateValues_ThenNothingChanges(t *test
 	assert.True(t, set.Get(50))
 	assert.True(t, set.Get(25))
 }
+
+func Test_GivenFullSet_WhenPuttingValues_ThenSizeDoesNotChangeAndMethodReturns(t *testing.T) {
+	// Given
+	set := Init[int]()
+	step, current := 7, 0
+
+	for i := 0; i < SET_SIZE; i++ {
+		set.Put(current)
+		current = (current + step) % SET_SIZE
+	}
+
+	// When
+	set.Put(100)
+	set.Put(-100)
+	set.Put(50)
+	set.Put(25)
+
+	// Then
+	assert.Equal(t, SET_SIZE, set.Size())
+}
