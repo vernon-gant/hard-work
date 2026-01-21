db7aa1070a173e4c4c7c18987b5bb15af609c67b
Aleksandr Zakharov <aleks.zakharov@pm.me>
2026-01-22 00:44:49 +0100

    .

--- algos1-go/10_set/solution_test.go
+++ algos1-go/10_set/solution_test.go
@@ -6,8 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// PUT TESTS
-
 func Test_GivenEmptySet_WhenPuttingSingleValue_ThenCanBeFound(t *testing.T) {
 	// Given
 	set := Init[int]()
@@ -18,4 +16,22 @@ func Test_GivenEmptySet_WhenPuttingSingleValue_ThenCanBeFound(t *testing.T) {
 	// Then
 	assert.Equal(t, 1, set.Size())
 	assert.True(t, set.Get(100))
+}
+
+func Test_GivenEmptySet_WhenPuttingMultipleValues_ThenTheyAllArePresent(t *testing.T) {
+	// Given
+	set := Init[int]()
+
+	// When
+	set.Put(100)
+	set.Put(-100)
+	set.Put(50)
+	set.Put(25)
+
+	// Then
+	assert.Equal(t, 4, set.Size())
+	assert.True(t, set.Get(100))
+	assert.True(t, set.Get(-100))
+	assert.True(t, set.Get(50))
+	assert.True(t, set.Get(25))
 }
