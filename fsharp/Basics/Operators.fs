36748120996724ff07c5658afcc80405ae9f13d4
Aleksandr Zakharov <aleksandr.zakharov@peoplex.at>
2026-06-02 21:04:18 +0200

    f#: operators

---
+++ fsharp/Basics/Operators.fs
@@ -0,0 +1,9 @@
+// 16.1
+let notDivisible (n, m) = m % n <> 0
+
+// 16.2
+let prime n =
+    if n < 2 then false
+    else
+        let limit = int (sqrt (float n))
+        seq { 2 .. limit } |> Seq.forall (fun d -> n % d <> 0)
