efa47317256abd48841822ed5971824e23e17d6f
Aleksandr Zakharov <aleks.zakharov@pm.me>
2026-06-05 10:00:13 +0200

    f#: string tasks

--- fsharp/Basics/Operators.fs
+++ fsharp/Basics/Operators.fs
@@ -1,9 +1,17 @@
 // 16.1
+open System
+
 let notDivisible (n, m) = m % n = 0
 
 // 16.2
 let prime n =
-    if n < 2 then false
+    if n < 2 then
+        false
     else
         let limit = int (sqrt (float n))
-        seq { 2 .. limit } |> Seq.forall (fun d -> n % d <> 0)
+        seq { 2..limit } |> Seq.forall (fun d -> n % d <> 0)
+
+let test x =
+    let a, b = x
+    match x with
+    | 0, 1 -> 1
