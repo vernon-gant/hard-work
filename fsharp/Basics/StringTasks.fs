29e899071943eb30afe295059bc2c6628107698b
Aleksandr Zakharov <aleksandr.zakharov@peoplex.at>
2026-06-05 11:40:15 +0200

    f#: fix string solution

--- fsharp/Basics/StringTasks.fs
+++ fsharp/Basics/StringTasks.fs
@@ -1,17 +1,16 @@
 ﻿// 17.1
-let rec pow text n =
+let rec pow (text, n) =
     match n with
     | n when n <= 0 -> ""
     | 1 -> text
-    | n -> text + pow text (n - 1)
-
+    | n -> text + pow (text, n - 1)
 
 // 17.2
-let rec isIthChar (text: string) position letter =
+let rec isIthChar (text: string, position, letter) =
     text.[position] = letter
 
 // 17.3
-let rec occFromIth (text: string) position letter =
+let rec occFromIth (text: string, position, letter) =
     let length = String.length text
     let rec occFromIthRec currentPosition acc =
         match currentPosition with
