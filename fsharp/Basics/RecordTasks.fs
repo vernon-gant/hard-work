78c1b8614058bc1a5c1cb1336363b60174e2871e
Aleksandr Zakharov <aleks.zakharov@pm.me>
2026-06-11 16:20:04 +0200

    f#: records

---
+++ fsharp/Basics/RecordTasks.fs
@@ -0,0 +1,8 @@
+﻿type TimeOfDay = { hours: int; minutes: int; f: string }
+
+let (.>.) x y =
+    let convert time =
+        match time.f with
+        | "AM" -> (time.hours, time.minutes)
+        | _ -> (time.hours + 12, time.minutes)
+    convert x > convert y
