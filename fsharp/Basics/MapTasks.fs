3e937d06f482aa44704f4c2f9c8dbed1af4d052d
Aleksandr Zakharov <aleksandr.zakharov@peoplex.at>
2026-06-29 17:15:36 +0200

    f#: self made binary search

--- fsharp/Basics/MapTasks.fs
+++ fsharp/Basics/MapTasks.fs
@@ -1,8 +1,17 @@
 // 43.3
-open System
-
 let try_find key m =
-    let mapArray = Map.toArray m 
-    match Array.BinarySearch(Array.map fst mapArray, key) with
-    | n when n < 0 -> None
-    | n -> Some (snd mapArray[n])
+    let entries = Map.toArray m
+
+    let rec search low high =
+        if low > high then
+            None
+        else
+            let mid = (low + high) / 2
+            let (entryKey, value) = entries[mid]
+
+            match compare key entryKey with
+            | 0 -> Some value
+            | c when c < 0 -> search low (mid - 1)
+            | _ -> search (mid + 1) high
+
+    search 0 (entries.Length - 1)
