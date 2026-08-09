0be59aa8436405cb5ce8a6a51fe3fbd0fda7589a
Aleksandr Zakharov <aleks.zakharov@pm.me>
2026-08-09 10:37:59 +0200

    task(algos3): bubble sort and selection sort

--- algos3/Algos3/Task1.cs
+++ algos3/Algos3/Task1.cs
@@ -4,11 +4,43 @@ public static class Task1
 {
     public static void SelectionSortStep(int[] array, int i)
     {
-        throw new NotImplementedException();
+        if (i >= array.Length - 1)
+            return;
+
+        var minIndex = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[minIndex] > array[j])
+                minIndex = j;
+        }
+
+        Swap(minIndex, i, array);
+    }
+
+    public static void Swap(int x, int y, int[] array)
+    {
+        var temp = array[x];
+        array[x] = array[y];
+        array[y] = temp;
     }
 
     public static bool BubbleSortStep(int[] array)
     {
-        throw new NotImplementedException();
+        if (array.Length < 2)
+            return true;
+
+        bool noSwaps = true;
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i - 1] > array[i])
+            {
+                Swap(i, i - 1, array);
+                noSwaps = false;
+            }
+        }
+
+        return noSwaps;
     }
 }
