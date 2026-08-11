4aff6a11b235a0376fd5db479c4ec3ba16a0a61d
Aleksandr Zakharov <aleksandr.zakharov@peoplex.at>
2026-08-11 11:51:04 +0200

    task(algos3-python): fix knuth sequence

--- algos3-python/src/Task3.py
+++ algos3-python/src/Task3.py
@@ -1,9 +1,6 @@
 def KnuthSequence(array_size):
     def knuth_rec(result):
-        next = 3 * result[0] + 1
-        if next >= array_size:
-            return result
-        else:
-            return knuth_rec([next] + result)
-            
-    return knuth_rec([1]) if array_size > 1 else []
+        next_step = 3 * result[0] + 1
+        return result if next_step >= array_size else knuth_rec([next_step] + result)
+
+    return knuth_rec([1]) if array_size > 0 else []
